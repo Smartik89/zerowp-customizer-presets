143b76f135f532d2a2468308376996416cf584b1
andrei <andrei@100danish.com>
2019-09-12 15:22:05 +0300

    Initial CI configuration in Github Actions

--- deploy.sh
+++ deploy.sh
@@ -1,51 +1,30 @@
 #!/usr/bin/env bash
 
 PLUGIN_SLUG="${PWD##*/}"
-
-echo "slug"
-echo $PLUGIN_SLUG
-
-echo "username"
-echo $SVN_USERNAME
-
-echo "password"
-echo $SVN_PASSWORD
-
-echo "repository"
-echo $GITHUB_REPOSITORY
-
-echo "workspace"
-echo $GITHUB_WORKSPACE
-
-echo "ref"
-echo $GITHUB_REF
-
 TAG=$(sed -e "s/refs\/tags\///g" <<< $GITHUB_REF)
 
-echo $TAG
+sed -i -e "s/__STABLE_TAG__/$TAG/g" ./src/readme.txt
+sed -i -e "s/__STABLE_TAG__/$TAG/g" ./src/plugin-base.php
+svn co --depth immediates "https://plugins.svn.wordpress.org/$PLUGIN_SLUG" ./svn
 
-#sed -i -e "s/__STABLE_TAG__/$TRAVIS_TAG/g" ./src/readme.txt
-#sed -i -e "s/__STABLE_TAG__/$TRAVIS_TAG/g" ./src/plugin-base.php
-#svn co --depth immediates "https://plugins.svn.wordpress.org/$PLUGIN_SLUG" ./svn
-#
-#svn update --set-depth infinity ./svn/trunk
-#svn update --set-depth infinity ./svn/assets
-#svn update --set-depth infinity ./svn/tags/$TRAVIS_TAG
-#
-#cp -R ./src/* ./svn/trunk
-#cp -R ./wp_org/assets/* ./svn/assets
-#
-## 3. Switch to SVN repository
-#cd ./svn
-#
-#svn add --force trunk
-#svn add --force assets
-#
-#svn cp trunk tags/$TRAVIS_TAG
-#
-#svn add --force tags
-#
-#svn ci  --message "Release $TRAVIS_TAG" \
-#        --username $SVN_USERNAME \
-#        --password $SVN_PASSWORD \
-#        --non-interactive
+svn update --set-depth infinity ./svn/trunk
+svn update --set-depth infinity ./svn/assets
+svn update --set-depth infinity ./svn/tags/$TAG
+
+cp -R ./src/* ./svn/trunk
+cp -R ./wp_org/assets/* ./svn/assets
+
+# 3. Switch to SVN repository
+cd ./svn
+
+svn add --force trunk
+svn add --force assets
+
+svn cp trunk tags/$TAG
+
+svn add --force tags
+
+svn ci  --message "Release $TAG" \
+        --username $SVN_USERNAME \
+        --password $SVN_PASSWORD \
+        --non-interactive
